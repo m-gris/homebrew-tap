14893592f8a36e8e9e60ca82471ceff09e2028a7
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2026-02-03 17:01:50 +0400

    fix: include hook executables in v0.1.1

--- Formula/psst.rb
+++ Formula/psst.rb
@@ -1,18 +1,20 @@
 class Psst < Formula
   desc "Nudges Claude Code to use just recipes instead of repetitive bash commands, reducing context waste"
   homepage "https://github.com/m-gris/psst"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/m-gris/psst/releases/download/v0.1.0/psst-0.1.0-darwin-arm64.tar.gz"
-      sha256 "167b7cfc589b1040f678f69b2a22c0e2aaf8d037ef88413fe8a509bc95632fb8"
+      url "https://github.com/m-gris/psst/releases/download/v0.1.1/psst-0.1.1-darwin-arm64.tar.gz"
+      sha256 "3e17c9876eda33c9a45df88c15ade97d69fca6afeffd4a5573286ff65658b8af"
     end
   end
 
   def install
     bin.install "psst"
+    bin.install "psst-pre-tool-hook"
+    bin.install "psst-post-tool-hook"
   end
 
   def caveats
